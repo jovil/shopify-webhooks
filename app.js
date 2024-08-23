5b03fb8160d7fa3ec8e027b49369cacf4c0549b9
Jovil <jovilh@gmail.com>
2024-08-23 20:03:04 +0800

    initial commit

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+const PORT = 3000;
+
+app.use(bodyParser.json());
+
+app.post("/webhook/orders/create", (req, res) => {
+  const orderData = req.body;
+  console.log("Order data received", orderData);
+  res.status(200).send("Webhook received");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running at port ${PORT}`);
+});
